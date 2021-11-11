38976066569073eac8eea5ef1f4d75b4519e380c
DanielElisenberg <daniel@elisenberg.com>
2021-11-11 14:51:22 +0100

    lift_and_shift_code: simple testing

---
+++ tests/test_validate.py
@@ -0,0 +1,53 @@
+import os
+import logging
+from microdata_validator import validate
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+VALID_DATASET_NAME = 'VALID_DATASET'
+INVALID_METADATA_DATASET_NAME = 'INVALID_METADATA_DATASET'
+INPUT_DIRECTORY = 'tests/resources/input_directory'
+WORKING_DIRECTORY = 'tests/resources/working_directory'
+
+def test_validate_valid_dataset():
+    data_errors = validate(
+        VALID_DATASET_NAME,
+        working_directory=WORKING_DIRECTORY,
+        input_directory=INPUT_DIRECTORY,
+        keep_generated_files=True
+    )
+    actual_files = get_working_directory_files()
+    expected_files = [
+        f'{VALID_DATASET_NAME}.db',
+        f'{VALID_DATASET_NAME}.json',
+        f'{VALID_DATASET_NAME}.csv'
+    ]
+    assert not data_errors
+    for file in expected_files:
+        assert file in actual_files
+
+
+def test_validate_invalid_metadata_dataset():
+    # improve error handling in reader
+    data_errors = validate(
+        INVALID_METADATA_DATASET_NAME,
+        working_directory=WORKING_DIRECTORY,
+        input_directory=INPUT_DIRECTORY,
+        keep_generated_files=True
+    )
+    temporary_files = get_working_directory_files()
+    assert not data_errors
+    assert temporary_files == []
+
+
+def get_working_directory_files() -> list:
+    return [
+        file for file in os.listdir(WORKING_DIRECTORY)
+        if file != '.gitkeep'
+    ]
+
+
+def teardown_function():
+    for file in get_working_directory_files():
+        if file != '.gitkeep':
+            os.remove(f'{WORKING_DIRECTORY}/{file}')
