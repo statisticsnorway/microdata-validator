6a8198558997ed82a5764de980906728350e935c
DanielElisenberg <daniel@elisenberg.com>
2021-11-11 14:49:47 +0100

    lift_and_shift_code: simple init file for package

--- microdata_validator/__init__.py
+++ microdata_validator/__init__.py
@@ -1,24 +1,32 @@
-import dataset_reader
-import dataset_validator
+from microdata_validator import dataset_reader
+from microdata_validator import dataset_validator
 import logging
+from pathlib import Path
+import uuid
+import os
+
 
 logger = logging.getLogger()
 
 
 def validate(dataset_name: str,
-             working_directory: str = '/',
+             working_directory: str = '',
+             input_directory: str = '',
+             keep_generated_files: bool = False,
              print_errors_to_file: bool = False) -> bool:
+    if working_directory:
+        working_directory_path = Path(working_directory)
+    else:
+        working_directory_path = Path(str(uuid.uuid4()))
+        os.mkdir(working_directory_path)
+    input_directory_path = Path(input_directory)
+
     try:
-        dataset_reader.run_reader(working_directory, dataset_name)
-        data_errors = dataset_validator.run_validator()
-        if data_errors:
-            print("There were data errors")
-            if print_errors_to_file:
-                print("Printing errors to file")
-            return False
-        else:
-            print("Dataset files are valid")
-            return True
+        dataset_reader.run_reader(working_directory_path, input_directory_path, dataset_name)
+        data_errors = dataset_validator.run_validator(working_directory_path, dataset_name)
+        if print_errors_to_file:
+            print("errors to file")
+        return data_errors
     except Exception as e:
         print(str(e))
 
