7c1a49dac9d1dee924540487c07bd3d6ef2f9d65
DanielElisenberg <daniel@elisenberg.com>
2022-03-11 16:34:48 +0100

    ref_reimplementation: more tests + restructure tests for inlining

--- tests/test_inline_metadata.py
+++ tests/test_inline_metadata.py
@@ -1,40 +1,58 @@
+from microdata_validator import inline_metadata
 from microdata_validator import utils
 from microdata_validator.utils import ParseMetadataError
 from pathlib import Path
 import pytest
+import os
+
 
 RESOURCES_DIR = "tests/resources/inline_metadata"
-SIVSTAND_REFERENCED_FILE_PATH = Path(f"{RESOURCES_DIR}/sivstand_referenced.json")
-SIVSTAND_INLINE_FILE_PATH = Path(f"{RESOURCES_DIR}/sivstand_inline.json")
-NONEXISTENT_FILE_PATH = Path(f"{RESOURCES_DIR}/does-not-exist.json")
-METADATA_FILE_PATH = Path(f"{RESOURCES_DIR}/simple_referenced.json")
-EXPECTED_METADATA_FILE_PATH = Path(f"{RESOURCES_DIR}/simple_inlined.json")
-REF_DIR = Path("tests/resources/ref_directory")
-
-
-def test_inline_simple():
-    actual_inlined = utils.inline_metadata_references(
-        METADATA_FILE_PATH, REF_DIR
-    )
-    assert actual_inlined == utils.load_json(
-       EXPECTED_METADATA_FILE_PATH
-    )
+OUTPUT_FILE_PATH = "tests/resources/my_inlined_file.json"
+KJOENN_FILE_PATH = (
+    "tests/resources/input_directory/"
+    "SYNT_BEFOLKNING_KJOENN/SYNT_BEFOLKNING_KJOENN.json"
+)
+KJOENN_DEFAULT_OUTPUT_PATH = (
+    "tests/resources/input_directory/"
+    "SYNT_BEFOLKNING_KJOENN/SYNT_BEFOLKNING_KJOENN_inlined.json"
+)
+KJOENN_INLINED_FILE_PATH = f"{RESOURCES_DIR}/SYNT_BEFOLKNING_KJOENN_inlined.json"
+SIVSTAND_REFERENCED_FILE_PATH = f"{RESOURCES_DIR}/sivstand_referenced.json"
+SIVSTAND_INLINE_FILE_PATH = f"{RESOURCES_DIR}/sivstand_inline.json"
+NONEXISTENT_FILE_PATH = f"{RESOURCES_DIR}/does-not-exist.json"
+REF_DIR = "tests/resources/ref_directory"
 
 
 def test_inline_full_dataset():
-    actual_inlined = utils.inline_metadata_references(
-        SIVSTAND_REFERENCED_FILE_PATH, REF_DIR
+    result_file_path = inline_metadata(
+        SIVSTAND_REFERENCED_FILE_PATH,
+        REF_DIR,
+        output_file_path=OUTPUT_FILE_PATH
     )
+    actual_inlined = utils.load_json(result_file_path)
     actual_inlined == utils.load_json(
-        SIVSTAND_INLINE_FILE_PATH
+        Path(SIVSTAND_INLINE_FILE_PATH)
     )
 
-
-def test_inline_invalid_metadata_file_path():
-    with pytest.raises(FileNotFoundError):
-        utils.inline_metadata_references(
-            NONEXISTENT_FILE_PATH, REF_DIR
+def test_inline_full_dataset_no_overwite():
+    with pytest.raises(FileExistsError):
+        inline_metadata(
+            SIVSTAND_REFERENCED_FILE_PATH,
+            REF_DIR
         )
+
+
+def test_inline_full_dataset_default_output_path():
+    result_file_path = inline_metadata(
+        KJOENN_FILE_PATH,
+        REF_DIR
+    )
+    assert str(result_file_path) == KJOENN_DEFAULT_OUTPUT_PATH  
+    actual_inlined = utils.load_json(result_file_path)
+    actual_inlined == utils.load_json(
+        Path(KJOENN_INLINED_FILE_PATH)
+    )
+
 
 def test_inline_invalid_ref_dir():
     with pytest.raises(ParseMetadataError) as e:
@@ -50,5 +68,12 @@ def test_inline_invalid_ref_dir():
     assert "Supplied reference directory is invalid" in str(e)
 
 
-def test_inline_invalid_ref():
-    pass
+def teardown_function():
+    try:
+        os.remove(OUTPUT_FILE_PATH)
+    except:
+        pass
+    try:
+        os.remove(KJOENN_DEFAULT_OUTPUT_PATH)
+    except:
+        pass 
