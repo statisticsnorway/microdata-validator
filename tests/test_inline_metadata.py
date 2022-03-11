f6b98d5312673657e3de50d6a5260dcdca3856a0
DanielElisenberg <daniel@elisenberg.com>
2022-03-11 14:06:31 +0100

    ref_reimplementation: tests

---
+++ tests/test_inline_metadata.py
@@ -0,0 +1,54 @@
+from microdata_validator import file_utils
+from microdata_validator.file_utils import ParseMetadataError
+from pathlib import Path
+import pytest
+
+RESOURCES_DIR = "tests/resources/inline_metadata"
+SIVSTAND_REFERENCED_FILE_PATH = Path(f"{RESOURCES_DIR}/sivstand_referenced.json")
+SIVSTAND_INLINE_FILE_PATH = Path(f"{RESOURCES_DIR}/sivstand_inline.json")
+NONEXISTENT_FILE_PATH = Path(f"{RESOURCES_DIR}/does-not-exist.json")
+METADATA_FILE_PATH = Path(f"{RESOURCES_DIR}/simple_referenced.json")
+EXPECTED_METADATA_FILE_PATH = Path(f"{RESOURCES_DIR}/simple_inlined.json")
+REF_DIR = Path("tests/resources/ref_directory")
+
+
+def test_inline_simple():
+    actual_inlined = file_utils.inline_metadata_references(
+        METADATA_FILE_PATH, REF_DIR
+    )
+    assert actual_inlined == file_utils.load_json(
+       EXPECTED_METADATA_FILE_PATH
+    )
+
+
+def test_inline_full_dataset():
+    actual_inlined = file_utils.inline_metadata_references(
+        SIVSTAND_REFERENCED_FILE_PATH, REF_DIR
+    )
+    actual_inlined == file_utils.load_json(
+        SIVSTAND_INLINE_FILE_PATH
+    )
+
+
+def test_inline_invalid_metadata_file_path():
+    with pytest.raises(FileNotFoundError):
+        file_utils.inline_metadata_references(
+            NONEXISTENT_FILE_PATH, REF_DIR
+        )
+
+def test_inline_invalid_ref_dir():
+    with pytest.raises(ParseMetadataError) as e:
+        file_utils.inline_metadata_references(
+            SIVSTAND_REFERENCED_FILE_PATH, None
+        )
+    assert "No supplied reference directory" in str(e)
+
+    with pytest.raises(ParseMetadataError) as e:
+        file_utils.inline_metadata_references(
+            SIVSTAND_REFERENCED_FILE_PATH, SIVSTAND_REFERENCED_FILE_PATH
+        )
+    assert "Supplied reference directory is invalid" in str(e)
+
+
+def test_inline_invalid_ref():
+    pass
