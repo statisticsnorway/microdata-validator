7abc1f3eeb462f4398d99d625f7d8aa6388e1f09
DanielElisenberg <daniel@elisenberg.com>
2022-05-31 20:12:07 +0200

    add_unit_type_info: unit type validation test

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -15,6 +15,7 @@ WRONG_DELIMITER_DATASET_NAME = 'WRONG_DELIMITER_DATASET'
 MISSING_IDENTIFIER_DATASET_NAME = 'MISSING_IDENTIFIER_DATASET'
 INVALID_DATES_DATASET_NAME = 'INVALID_DATES_DATASET'
 INVALID_DATE_RANGES_DATASET_NAME = 'INVALID_DATE_RANGES_DATASET'
+INVALID_UNIT_TYPE_DATASET_NAME = 'INVALID_UNIT_TYPE_DATASET'
 INPUT_DIRECTORY = 'tests/resources/input_directory'
 WORKING_DIRECTORY = 'tests/resources/working_directory'
 REF_DIRECTORY = 'tests/resources/ref_directory'
@@ -136,6 +137,23 @@ def test_validate_invalid_dates():
     assert data_errors == [
         "row 4: STOP date not valid - '1926-01-01'",
         "row 15: STOP date not valid - '1940-02-01'"
+    ]
+
+
+def test_validate_invalid_unit_type():
+    data_errors = validate(
+        INVALID_UNIT_TYPE_DATASET_NAME,
+        working_directory=WORKING_DIRECTORY,
+        input_directory=INPUT_DIRECTORY,
+    )
+    assert data_errors == [
+        (
+            "properties.identifierVariables.items.properties.unitType."
+            "properties.shortName.enum: "
+            "'SOMETHING' is not one of ['JOBB', 'KJORETOY', 'FAMILIE', "
+            "'FORETAK', 'HUSHOLDNING', 'KOMMUNE', 'KURS', 'PERSON', "
+            "'VIRKSOMHET']"
+        )
     ]
 
 
