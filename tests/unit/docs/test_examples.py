552c2bc3868fea73807a92517c3dc2737a352e0f
DanielElisenberg <Daniel@Elisenberg.com>
2022-11-11 13:32:52 +0100

    validate_examples: validate example datasets in unit test

---
+++ tests/unit/docs/test_examples.py
@@ -0,0 +1,27 @@
+import json
+import os
+import logging
+
+import pytest
+
+from microdata_validator import validate
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+INPUT_DIRECTORY = 'docs/examples'
+EXAMPLE_DATASETS = [
+    'SYNT_BEFOLKNING_KJOENN',
+    'SYNT_BEFOLKNING_SIVSTAND',
+    'SYNT_PERSON_INNTEKT'
+]
+
+
+def test_validate_valid_dataset():
+    for dataset_name in EXAMPLE_DATASETS:
+        data_errors = validate(
+            dataset_name,
+            input_directory=INPUT_DIRECTORY
+        )
+        assert not data_errors
