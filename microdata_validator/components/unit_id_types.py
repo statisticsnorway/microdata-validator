a6e73a73973a8657ec926eac32c21e5913376a13
DanielElisenberg <Daniel@Elisenberg.com>
2023-02-16 16:01:39 +0100

    pydantic_refactor_plus: housekeeping

--- microdata_validator/components/unit_id_types.py
+++ microdata_validator/components/unit_id_types.py
@@ -3,18 +3,18 @@ from typing import Union
 from microdata_validator.exceptions import UnregisteredUnitTypeError
 
 # When updating this dictionary remember to also
-# update the DatasetMetadataSchema with the
-# same key value in the enum for unitTypeType
+# update the Metadata model with the
+# same key value in the enum for unitType
 UNIT_ID_TYPE_FOR_UNIT_TYPE = {
-    "JOBB": "JOBBID_1",
-    "KJORETOY": "KJORETOY_ID",
-    "FAMILIE": "FNR",
-    "FORETAK": "ORGNR",
-    "HUSHOLDNING": "FNR",
-    "KOMMUNE": None,
-    "KURS": "KURSID",
-    "PERSON": "FNR",
-    "VIRKSOMHET": "ORGNR"
+    'JOBB': 'JOBBID_1',
+    'KJORETOY': 'KJORETOY_ID',
+    'FAMILIE': 'FNR',
+    'FORETAK': 'ORGNR',
+    'HUSHOLDNING': 'FNR',
+    'KOMMUNE': None,
+    'KURS': 'KURSID',
+    'PERSON': 'FNR',
+    'VIRKSOMHET': 'ORGNR'
 }
 
 
