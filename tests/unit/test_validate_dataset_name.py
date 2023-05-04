b31232c7714d8a02d7e911976b795ce332213904
DanielElisenberg <Daniel@Elisenberg.com>
2023-05-04 11:32:47 +0200

    more_name_validation: extra validation for names

--- tests/unit/test_validate_dataset_name.py
+++ tests/unit/test_validate_dataset_name.py
@@ -7,8 +7,8 @@ from microdata_validator.exceptions import InvalidDatasetName
 def test_validate_dataset_name():
     validate_dataset_name('MITT_DATASET')
     validate_dataset_name('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    validate_dataset_name('123456789')
-    validate_dataset_name('1A_2Z3_334_567_GHJ')
+    validate_dataset_name('A123456789')
+    validate_dataset_name('B1A_2Z3_334_567_GHJ')
 
     with pytest.raises(InvalidDatasetName):
         validate_dataset_name('_LEADING_UNDERSCORE')
